d00f9264c55e7f89d1148b98559a693dba5a687d
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-06-30 05:21:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+  "He hated the sound of his own voice"
+];
   
 function johnLennonFacts(facts){
   let j=0
