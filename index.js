ca2b96b8fbd4a67766fca55818486798c2933cd3
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-06-30 05:38:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function theBeatlesPlay(musicians, instruments){
   return array;
 }
 
-function johnLennonFacts(facts){
+function johnLennonFacts(eFacts){
 const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
-  
+  while
 }
