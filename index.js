7a0ae38ec3f840675b882c3c81d535d65e9e8a9f
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-06-30 05:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ function theBeatlesPlay(musicians, instruments){
 
 
 function johnLennonFacts(){
-var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  var facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"];
   var newArray = [];
   var j =0;
   while(j<facts.length){
@@ -26,3 +25,13 @@ var facts = [
   return newArray;
 }
 
+function johnLennonFacts() {
++    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
++    var newFacts = [];
++    var i = 0;
++    while (i < facts.length) {
++        newFacts.push(facts[i] +"!!!");
++        i++;
++    }
++    return newFacts;
++}
